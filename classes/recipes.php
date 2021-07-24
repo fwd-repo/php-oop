09e6a0755add13ce57b9f9aa4b257dfbefe97465
Ed Tansey <office.fwd@yahoo.com>
2021-07-24 14:05:50 +0100

    Apply access modifiers

--- classes/recipes.php
+++ classes/recipes.php
@@ -2,13 +2,23 @@
 
 class Recipe
 {
-	public $title;
+	private $title;
 	public $ingredients = array();
 	public $instructions = array();
 	public $yield;
 	public $tag = array();
 	public $source = "Ed T";
-
+  
+  public function setTitle($title)
+  {
+    $this->title = ucwords($title); 
+  }
+  
+  public function getTitle()
+  {
+    return $this->title; 
+  }
+  
 	public function displayRecipe()
 	{
 		return $this->title . " by " . $this->source;
@@ -18,11 +28,12 @@ class Recipe
 
 $recipe1 = new Recipe();
 $recipe1->source = "Delia Smith";
-$recipe1->title = "My First Recipe";
+$recipe1->setTitle("my first recipe");
 
 $recipe2 = new Recipe();
 $recipe2->source = "Jamie Oliver";
-$recipe2->title = "My Second Recipe";
+$recipe2->setTitle("my second recipe");
 
+echo $recipe1->getTitle();
 echo $recipe1->displayRecipe();
 echo $recipe2->displayRecipe();
