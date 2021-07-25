95a628b0687ec7c397c58418e64959dc58331450
Ed Tansey <office.fwd@yahoo.com>
2021-07-25 18:25:37 +0100

    Separating static methods in the Render class

--- classes/render.php
+++ classes/render.php
@@ -2,17 +2,24 @@
 
 class Render
 {
-	public static function displayRecipe($recipe)
+	public static function listIngredients($ingredients)
+  	{
+    	$output = "";
+    	foreach ($ingredients as $ing) {
+      		$output .= $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
+      		$output .= "\n";
+    	}
+    	return $output;
+  	}
+  
+  	public static function displayRecipe($recipe)
 	{
     	$output = "";
     	$output .= $recipe->getTitle() . " by " . $recipe->getSource();
     	$output .= "\n";
     	$output .= implode(", ",$recipe->getTags());
     	$output .= "\n\n";
-    	foreach ($recipe->getIngredients() as $ing) {
-      		$output .= $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
-      		$output .= "\n";
-    	}
+    	$output .= self::listIngredients($recipe->getIngredients());
     	$output .= "\n";
     	$output .= implode("\n", $recipe->getInstructions());
     	$output .= "\n";
