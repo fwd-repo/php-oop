422af672aee144c7f073849893e42efa5b41dd49
Ed Tansey <office.fwd@yahoo.com>
2021-07-25 16:53:27 +0100

    Add setters and getters to recipe class

--- classes/recipes.php
+++ classes/recipes.php
@@ -3,13 +3,13 @@
 class Recipe
 {
 	private $title;
-	public $ingredients = array();
-	public $instructions = array();
-	public $yield;
-	public $tag = array();
-	public $source = "Ed T";
+	private $ingredients = array();
+	private $instructions = array();
+	private $yield;
+	private $tag = array();
+	private $source = "Ed T";
   
-  private $measurements = array(
+  	private $measurements = array(
 		"tsp",
 		"tbsp",
 		"cup",
@@ -21,35 +21,76 @@ class Recipe
 		"gallon"
 	);
   
-  public function setTitle($title)
-  {
-    $this->title = ucwords($title); 
-  }
+  	public function setTitle($title)
+  	{
+    	$this->title = ucwords($title); 
+  	}
   
-  public function getTitle()
-  {
-    return $this->title; 
-  }
+  	public function getTitle()
+  	{
+    	return $this->title; 
+  	}
   
-  public function addIngredient($item, $amount = null, $measure = null)
-  {
-    if ($amount != null && !is_float($amount) && !is_int($amount)) {
-      exit("The amount must be a float: " . gettype($amount) . " $amount given");
-    }
-    if ($measure != null && !in_array(strtolower($measure), $this->measurements)) {
-      exit("Please enter a valid measurement: " . implode(", ", $this->measurements));
-    }
-    $this->ingredients[] = array(
-      "item" => ucwords($item),
-      "amount" => $amount,
-      "measure" => strtolower($measure)
-    );
-  }
+  	public function addIngredient($item, $amount = null, $measure = null)
+  	{
+    	if ($amount != null && !is_float($amount) && !is_int($amount)) {
+      		exit("The amount must be a float: " . gettype($amount) . " $amount given");
+    	}
+
+    	if ($measure != null && !in_array(strtolower($measure), $this->measurements)) {
+      		exit("Please enter a valid measurement: " . implode(", ", $this->measurements));
+    	}
+
+    	$this->ingredients[] = array(
+      		"item" => ucwords($item),
+      		"amount" => $amount,
+      		"measure" => strtolower($measure)
+    	);
+ 	}
   
-  public function getIngredients()
-  {
-    return $this->ingredients;
-  }
+  	public function addInstruction($string)
+  	{
+    	$this->instructions[] = $string; 
+  	}
+  
+  	public function getInstructions()
+  	{
+    	return $this->instructions; 
+  	}
+  
+  	public function addTag($tag)
+  	{
+    	$this->tags[] = strtolower($tag); 
+  	}
+  
+  	public function getTags()
+  	{
+    	return $this->tags; 
+  	}
+  
+  	public function setYield($yield)
+  	{
+    	$this->yield = $yield;
+  	}
+  
+  	public function getYield() {
+    	return $this->yield;
+  	}
+  
+  	public function setSource($source)
+  	{
+    	$this->source = ucwords($source);
+  	}
+  
+  	public function getSource()
+  	{
+    	return $this->source;
+  	}
+
+  	public function getIngredients()
+  	{
+    	return $this->ingredients;
+  	}
   
 	public function displayRecipe()
 	{
--- cookbook.php
+++ cookbook.php
@@ -2,16 +2,31 @@
 include "classes/recipe.php";
 
 $recipe1 = new Recipe();
-$recipe1->source = "Delia Smith";
+$recipe1->setSource = "Delia Smith";
 $recipe1->setTitle("my first recipe");
 $recipe1->addIngredient("egg", 1);
 $recipe1->addIngredient("flour",2,"cup");
 
 $recipe2 = new Recipe();
-$recipe2->source = "Jamie Oliver";
+$recipe2->setSource = "Jamie Oliver";
 $recipe2->setTitle("my second recipe");
 
 echo $recipe1->getTitle();
+
 foreach ($recipe1->getIngredients() as $ing) {
-  echo "\n" . $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
+	echo "\n" . $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
 }
+
+$recipe1->addInstruction("This is my first instruction");
+$recipe1->addInstruction("This is my second instruction");
+
+echo implode("\n", $recipe1->getInstructions());
+
+$recipe1->addTag("Breakfast");
+$recipe1->addTag("Main Course");
+
+echo implode(", ", $recipe1->getTags());
+
+$recipe1->setYield("6 servings");
+echo $recipe1->getYield();
+echo $recipe1->getSource();
