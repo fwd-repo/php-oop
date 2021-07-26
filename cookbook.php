471a1d75d202fd1d95623069901a0318a9c4b971
Ed Tansey <office.fwd@yahoo.com>
2021-07-26 17:09:14 +0100

    Add all recipe objects

--- cookbook.php
+++ cookbook.php
@@ -1,23 +1,6 @@
 <?php
 include "classes/recipe.php";
 include "classes/render.php";
+include "inc/allrecipes.php";
 
-$recipe1 = new Recipe();
-$recipe1->setSource = "Delia Smith";
-$recipe1->addIngredient("egg", 1);
-$recipe1->addIngredient("flour",2,"cup");
-
-$recipe2 = new Recipe();
-$recipe2->setSource = "Jamie Oliver";
-
-$recipe1->addInstruction("This is my first instruction");
-$recipe1->addInstruction("This is my second instruction");
-
-$recipe1->addTag("Breakfast");
-$recipe1->addTag("Main Course");
-
-$recipe1->setYield("6 servings");
-
-echo $recipe1;
-echo new Render();
-//echo Render::displayRecipe($recipe1);
+echo Render::displayRecipe($belgian_waffles);
